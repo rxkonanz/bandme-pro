67f3f22b98f47e90541b471942c60957412a2d62
Roberto Konanz <robkonanz@gmail.com>
2019-03-22 00:17:53 -0400

    Changes landing page title

--- client/src/components/pages/LandingPage.jsx
+++ client/src/components/pages/LandingPage.jsx
@@ -6,7 +6,7 @@ export default class LandingPage extends Component {
   render() {
     return (
       <div className="landing-page" className="landing-page">
-        <h1 className="landing-page-title">connect musicians => bands.</h1>
+        <h1 className="landing-page-title">connecting musicians => bands.</h1>
       </div>
     )
   }
