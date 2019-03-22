cb1b427a77f83da85d78f078a263bf7bfe4154c6
Roberto Konanz <robkonanz@gmail.com>
2019-03-21 23:27:52 -0400

    Like functionality working on drummers as well. Only missing guitarists.

--- client/src/components/pages/Drummers.jsx
+++ client/src/components/pages/Drummers.jsx
@@ -8,11 +8,13 @@ export default class Drummers extends Component {
 
     state = {
         allDrummers: [],
-        currentUser: localStorage.userEmail
+        currentUser: localStorage.userEmail,
+        likedMusicians: []
     }
 
     componentDidMount() {
         this.getDrummers();
+        this.getLikedMusicians();
     }
 
     getDrummers = () => {
@@ -25,21 +27,42 @@ export default class Drummers extends Component {
         })
     }
 
+    getLikedMusicians = () => {
+        Axios.post(`${SERVER_URL}/liked-musicians`, {currentUser: this.state.currentUser})
+        .then(res => {
+            this.setState({
+                likedMusicians: res.data.musicians[0].likedMusicians
+            })
+        })
+    }
+
     likeVideo = (musicianEmail) => {
         alert("You liked this video!");
         Axios.post(`${SERVER_URL}/like-video`, {bandEmail: this.state.currentUser, musicianEmail})
             .then(res => {
                 console.log(res)
             })
+        window.location.reload();
     }
 
     showDrummers = () => {
         let result = this.state.allDrummers.map((drummer,i) => {
-          return (<div className="each-musician col-xl-6 col-lg-6">
-                    <iframe className="youtube-video" title="youtubevideo" src={drummer.ytLink} frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-                    <p>{drummer.name}</p>
-                    <button onClick={(e) => this.likeVideo(drummer.email)}>Like</button>
-                  </div>)
+
+            if(this.state.likedMusicians.includes(drummer.email)){
+                return (<div className="each-musician col-xl-6 col-lg-6">
+                            <iframe className="youtube-video" title="youtubevideo" src={drummer.ytLink} frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                            <p>{drummer.name}</p>
+                            <button className="liked-button" onClick={(e) => this.likeVideo(drummer.email)}>Liked</button>
+                        </div>)
+            }
+            else {
+                return (<div className="each-musician col-xl-6 col-lg-6">
+                            <iframe className="youtube-video" title="youtubevideo" src={drummer.ytLink} frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                            <p>{drummer.name}</p>
+                            <button onClick={(e) => this.likeVideo(drummer.email)}>Like</button>
+                        </div>)
+            }
+
         })
         return result
       }
